328ac27c2b3ebcbd1f0a31cfefbb57c91ef52fa2
Lavin <adventurelw@gmail.com>
2013-04-23 00:11:26 +0800

    Refactor three static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,21 +4,36 @@ describe "StaticPages" do
   context "Home page" do
     it "should have content 'Tutorial App'" do
       visit '/static_pages/home'
-      page.should have_content('Tutorial App')
+      page.should have_selector('h1', 'Tutorial App')
+    end
+
+    it "should have the right title" do
+      visit "/static_pages/home"
+      page.should have_title("Ruby on Rails Tutorial App | Home")
     end
   end
 
   context "Help Page" do
     it "should have content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', 'Help')
+    end
+
+    it "should have the right title" do
+      visit "/static_pages/help"
+      page.should have_title("Ruby on Rails Tutorial App | Help")
     end
   end
 
   context "About Page" do
     it "should have content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', 'About Us')
+    end
+
+    it "should have the right title" do
+      visit "/static_pages/about"
+      page.should have_title("Ruby on Rails Tutorial App | About")
     end
   end
 end
