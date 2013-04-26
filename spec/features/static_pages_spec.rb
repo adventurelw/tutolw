875b1e83b87a65d4d03a9fa75c68a7de5a825b0c
Lavin <adventurelw@gmail.com>
2013-04-26 22:45:06 +0800

    use bootstrap; use restful routes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 feature "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial App" }
   context "Home page" do
-    background { visit "/static_pages/home" }
-    scenario "have content 'Tutorial App'" do
+    background { visit root_path }
+    scenario "have the h1 'Tutorial App'" do
       #page.should have_selector('h1', 'Tutorial App')
       expect(page).to have_selector('h1', 'Tutorial App')
     end
@@ -19,24 +19,35 @@ feature "StaticPages" do
   end
 
   context "Help Page" do
-    background { visit "/static_pages/help"}
-    scenario "have content 'Help'" do
+    background { visit help_path}
+    scenario "have the h1 'Help'" do
       expect(page).to have_selector('h1', 'Help')
     end
 
-    scenario "have the right title" do
+    scenario "have the title 'Help'" do
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   context "About Page" do
-    background { visit "/static_pages/about" }
-    scenario "have content 'About Us'" do
+    background { visit about_path }
+    scenario "have the h1 'About Us'" do
       expect(page).to have_selector('h1', 'About Us')
     end
 
-    scenario "have the right title" do
+    scenario "have the title 'About'" do
       expect(page).to have_title("#{base_title} | About")
+    end
+  end
+
+  context "Contact Page" do
+    background {visit contact_path}
+    scenario "have the h1 'Contact Us'" do
+      expect(page).to have_selector('h1', 'Contact Us')
+    end
+
+    scenario "have the title 'Contact'" do
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
